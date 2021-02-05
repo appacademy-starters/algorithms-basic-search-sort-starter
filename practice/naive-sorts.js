14e747eb667600a273053614deaba943a2e45a9c
Derek Jou <derekjou0@gmail.com>
2021-02-05 15:19:04 -0800

    initial commit for monday

---
+++ practice/naive-sorts.js
@@ -0,0 +1,29 @@
+// Implement Bubble Sort 
+const bubbleSort = (arr) => {
+
+};
+
+
+// Implement Selection Sort
+const selectionSort = (arr) => {
+
+};
+
+
+// Implement Insertion Sort 
+const insertionSort = (arr) => {
+
+};
+
+
+// Compare runtimes 
+
+
+
+
+
+module.exports = {
+    bubbleSort,
+    selectionSort,
+    insertionSort
+}
